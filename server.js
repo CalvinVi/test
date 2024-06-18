26f5de4ad656e7f63728feed82c1c10b18542aaa
s0579111 <s0579111@htw-berlin.de>
2024-06-18 22:20:57 +0200

    verschoben

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const api = require('./api');
+const api = require('./src/api');
 
 const app = express();
 const port = process.env.PORT || 8080;
