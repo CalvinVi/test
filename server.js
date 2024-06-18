df8324f1ebfa3c2160ba2bf001352d0e65935e1e
s0579111 <s0579111@htw-berlin.de>
2024-06-18 21:36:18 +0200

    server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const api = require('./api');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// API-Routen
+app.use('/api', api);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
