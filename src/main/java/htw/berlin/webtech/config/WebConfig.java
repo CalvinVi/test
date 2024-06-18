683baa0652acedde1a81a0ca282dfe6f9115f34c
s0579111 <s0579111@htw-berlin.de>
2024-06-18 02:15:34 +0200

    Problembehebung

--- src/main/java/htw/berlin/webtech/config/WebConfig.java
+++ src/main/java/htw/berlin/webtech/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://todofrontend-2.onrender.com") // Ersetze dies mit der URL deines Frontends
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
