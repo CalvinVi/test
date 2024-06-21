be1933a097fa443350262931d208e62557ea1efe
s0579111 <s0579111@htw-berlin.de>
2024-06-21 13:59:49 +0200

    Verzweiflung

--- src/main/java/htw/berlin/webtech/model/Todo.java
+++ src/main/java/htw/berlin/webtech/model/Todo.java
@@ -12,6 +12,7 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private String description;
     private boolean completed;
 
     // Getter und Setter
@@ -29,6 +30,14 @@ public class Todo {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public boolean isCompleted() {
--- src/main/java/htw/berlin/webtech/service/TodoService.java
+++ src/main/java/htw/berlin/webtech/service/TodoService.java
@@ -2,7 +2,6 @@ package htw.berlin.webtech.service;
 
 import htw.berlin.webtech.model.Todo;
 import htw.berlin.webtech.repository.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +11,29 @@ public class TodoService {
 
     private final TodoRepository todoRepository;
 
-    @Autowired
+    // @Autowired ist hier nicht notwendig
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
     public List<Todo> getAllTodos() {
         return todoRepository.findAll();
+    }
+
+    public Todo saveTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public Todo updateTodo(Long id, Todo todo) {
+        if (todoRepository.existsById(id)) {
+            todo.setId(id);
+            return todoRepository.save(todo);
+        } else {
+            throw new IllegalArgumentException("Todo not found with id " + id);
+        }
+    }
+
+    public void deleteTodo(Long id) {
+        todoRepository.deleteById(id);
     }
 }
