e9056ed300f92b203064d3ae4aec420cc82a4a1f
Calvin Vi <Calvin.Vi@Student.HTW-Berlin.de>
2024-06-25 23:30:25 +0200

    commite

--- src/test/java/htw/berlin/webtech/WebtechApplicationTest.java
+++ src/test/java/htw/berlin/webtech/WebtechApplicationTest.java
@@ -3,6 +3,7 @@ package htw.berlin.webtech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
